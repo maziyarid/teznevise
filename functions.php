47168d749b9ad69df4fcc923587ac1d06e10f002
Maziyar Moradi <49054922+maziyarid@users.noreply.github.com>
2026-08-17 20:09:32 +0330

    chore(v1.2.1): include promote-assets module

--- functions.php
+++ functions.php
@@ -3,14 +3,14 @@
  * Teznevise theme functions and definitions.
  *
  * @package Teznevise
- * @version 1.2.0
+ * @version 1.2.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'TEZNEVISE_VERSION', '1.2.0' );
+define( 'TEZNEVISE_VERSION', '1.2.1' );
 define( 'TEZNEVISE_DIR', get_template_directory() );
 define( 'TEZNEVISE_URI', get_template_directory_uri() );
 
@@ -123,7 +123,6 @@ function teznevise_enqueue_assets() {
 		}
 	}
 
-	// Service-specific CSS by slug (works with shared page-service.php template).
 	$service_css = array(
 		'service-thesis'     => 'service-thesis',
 		'service-proposal'   => 'service-proposal',
@@ -184,6 +183,7 @@ $teznevise_includes = array(
 	'/inc/customizer.php',
 	'/inc/page-meta.php',
 	'/inc/setup-pages.php',
+	'/inc/promote-assets.php',
 	'/inc/seo.php',
 );
 
