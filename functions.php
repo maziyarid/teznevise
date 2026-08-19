de01346944581d788feae3a6e7aa207129e28741
Maziyar Moradi <49054922+maziyarid@users.noreply.github.com>
2026-08-19 14:48:13 +0330

    feat: enqueue nav-touch.css/js for touch-tap submenu support

--- functions.php
+++ functions.php
@@ -86,7 +86,9 @@ wp_enqueue_style( 'teznevise-site-polish', TEZNEVISE_URI . '/assets/css/site-pol
 wp_enqueue_style( 'teznevise-header-fix', TEZNEVISE_URI . '/assets/css/header-fix.css', array( 'teznevise-site-polish' ), TEZNEVISE_VERSION );
 wp_enqueue_style( 'teznevise-mobile-fixes', TEZNEVISE_URI . '/assets/css/mobile-fixes.css', array( 'teznevise-header-fix' ), TEZNEVISE_VERSION );
 wp_enqueue_style( 'teznevise-blog', TEZNEVISE_URI . '/assets/css/blog.css', array( 'teznevise-mobile-fixes' ), TEZNEVISE_VERSION );
+wp_enqueue_style( 'teznevise-nav-touch', TEZNEVISE_URI . '/assets/css/nav-touch.css', array( 'teznevise-blog' ), TEZNEVISE_VERSION );
 wp_enqueue_script( 'teznevise-redesign', TEZNEVISE_URI . '/assets/js/redesign.js', array(), TEZNEVISE_VERSION, true );
 wp_enqueue_script( 'teznevise-main', TEZNEVISE_URI . '/assets/js/main.js', array( 'teznevise-redesign' ), TEZNEVISE_VERSION, true );
+wp_enqueue_script( 'teznevise-nav-touch', TEZNEVISE_URI . '/assets/js/nav-touch.js', array( 'teznevise-main' ), TEZNEVISE_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'teznevise_enqueue_assets' );
