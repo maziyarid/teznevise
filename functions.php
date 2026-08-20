28d83faa02f4332433d07495bcaf7e000bd63846
Maziyar ID <tehpars@live.com>
2026-08-20 14:53:55 +0330

    chore: bump TEZNEVISE_VERSION to 1.6.5 (sync with style.css / README / changelog from #418)

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Teznevise
  */
 if ( ! defined( 'ABSPATH' ) ) { exit; }
-define( 'TEZNEVISE_VERSION', '1.6.4' );
+define( 'TEZNEVISE_VERSION', '1.6.5' );
 define( 'TEZNEVISE_DIR', get_template_directory() );
 define( 'TEZNEVISE_URI', get_template_directory_uri() );
 
